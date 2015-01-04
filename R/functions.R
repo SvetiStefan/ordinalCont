7f1a13cd9c8d002252e7222944e533a2273cd410
maurizio-manuguerra <maurizio.manuguerra@gmail.com>
2015-01-05 09:51:05 +1100

    First structure

---
+++ R/functions.R
@@ -0,0 +1,12 @@
+#' A Function
+#'
+#' This function allows you to …
+#' @param a The param of the function, any number will do. Defaults to 0.
+#' @keywords useless
+#' @export
+#' @examples
+#' test_function()
+
+test_function <- function(a=0){
+	print(a)
+}
