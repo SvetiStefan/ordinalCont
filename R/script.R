3fa18daf94b31903f1731167abf3353e8b12f4e1
maurizio-manuguerra <maurizio.manuguerra@gmail.com>
2015-01-07 11:39:36 +1100

    Goal 1 code working. No package refinement

--- R/script.R
+++ R/script.R
@@ -18,7 +18,14 @@ B = 0.43
 T = 0.85
 pars=c(beta,M, B, T)
 
-v=z$vas
-covs=c(2,3)  #### fix this! Has to be laser*time + other covs
-x=z[covs]
+v=z$vas/10
+v[v==0]=0.001
+v[v==1]=0.999
+#covs=c(2,3)  #### fix this! Has to be laser*time + other covs
+#x=z[covs]
+xx=z$time*z$laser
+x1=sapply(xx==1,ifelse,1,0)
+x2=sapply(xx==2,ifelse,1,0)
+x3=sapply(xx==3,ifelse,1,0)
+x=cbind(x1,x2,x3)
 optim(pars,contOrdEst, v=v, x=x)
