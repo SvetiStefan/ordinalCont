ea6ec1c0ecf045e127ae955593f4d5a36b7032af
maurizio-manuguerra <maurizio.manuguerra@gmail.com>
2015-01-09 11:35:42 +1100

    Goal 1: package working (documentation had to be enlarged). Compares similarly to first example of clm package (wine) when recoding all the variables.

--- R/functions.R
+++ R/functions.R
@@ -1,81 +1,93 @@
 #' Generalized logistic g function
 #'
 #' This function compute a parametric version of the g function following Richards (1959): g(v) = M + 1/B * log(Tv^T/(1-v^T))
-#' @param v Vector of standarized scores from the continuous ordinal scale
-#' @param M The offset of the curve
-#' @param B The slope of the curve
-#' @param T The symmetry of the curve
-#' @keywords Richards, generalized logistic function
+#' @param v Vector of standarized scores from the continuous ordinal scale.
+#' @param par Vector of M, the offset of the curve; B, the slope of the curve, and T, the symmetry of the curve.
+#' @keywords Richards, generalized logistic function.
 #' @export
 #' @examples
 #' g_glf()
 
-g_glf <- function(v, M, B, T){
-  return(M+log(T*v^T/(1-v^T))/B)
+g_glf <- function(v, par){
+  #par = c(M, B, T)
+  return(par[1]+log(par[3]*v^par[3]/(1-v^par[3]))/par[2])
 }
 
 
 #' Derivative of generalized logistic g function
 #'
 #' This function compute a parametric version of the g function following Richards (1959): dg(v)/dv = T/B * 1/[v*(1-v^T)]
-#' @param v Vector of standarized scores from the continuous ordinal scale
-#' @param B The slope of the curve
-#' @param T The symmetry of the curve
-#' @keywords Richards, derivative, generalized logistic function
+#' @param v Vector of standarized scores from the continuous ordinal scale.
+#' @param par Vector of B, the slope of the curve, and T, the symmetry of the curve.
+#' @keywords Richards, derivative, generalized logistic function.
 #' @export
 #' @examples
 #' dg_glf()
 
-dg_glf <- function(v, B, T){
-  return(T/B/v/(1-v^T))
+dg_glf <- function(v, par){
+  #par = c(B, T)
+  return(par[2]/par[1]/v/(1-v^par[2]))
 }
 
 
 #' Log-likelihood function for single subject using the generalized logistic function as g function and without random effects
 #'
 #' This function compute the Log-likelihood function for single subject using the generalized logistic function as g function and without random effects.
-#' @param x Design matrix (fixed effects)
-#' @param beta Regression coefficients
-#' @param v Vector of standarized scores from the continuous ordinal scale
-#' @param M The offset of the curve
-#' @param B The slope of the curve
-#' @param T The symmetry of the curve
-#' @keywords likelihood, log-likelihood
+#' @param x Design matrix (fixed effects).
+#' @param beta Regression coefficients.
+#' @param v Vector of standarized scores from the continuous ordinal scale.
+#' @param M The offset of the curve.
+#' @param B The slope of the curve.
+#' @param T The symmetry of the curve.
+#' @keywords likelihood, log-likelihood.
 #' @export
 #' @examples
-#' loglik_glf()
+#' negloglik_glf()
 
-negloglik_glf <- function(par, v, x){
-  beta <- par[1:3]
-  M <- par[4]
-  B <- par[5]
-  T <- par[6]
-  
-  g <- g_glf(v, M, B, T)
-  dg <- dg_glf(v, B, T)
+negloglik_glf <- function(par, v, x, len_beta){
+  beta <- par[1:len_beta]
+  par_g <- par[(len_beta+1):(len_beta+3)]
+  par_dg <- par[(len_beta+2):(len_beta+3)]
+  #print(par)
+  #print(beta)
+  #print(par_g)
+  #print(par_dg)
+  g <- g_glf(v, par_g)
+  dg <- dg_glf(v, par_dg)
+  #print(g)
+  #print(dg)
   if (any(dg<=0)) return(Inf)
   xb <- x %*% beta
   return(-sum(log(dg) + g + xb -2*log(1+exp(g+xb))))
 }
 
-contOrdEst <- function(par, v, x){
-  fit <- optim(par,negloglik_glf, v=v, x=x)
+contOrdEst <- function(start, v, x){
+  len_beta <- ncol(x)
+  fit <- optim(par=start,negloglik_glf, v=v, x=x, len_beta=len_beta, method="BFGS", hessian = TRUE)
   ## compute QR-decomposition of x
-  qx <- qr(x)
+  #qx <- qr(x)
+  H=fit$hessian
+  qrH <- qr(H)
+  if(qrH$rank < nrow(H))
+    stop("Cannot compute vcov: \nHessian is numerically singular")
+  vcov <- solve.qr(qrH)
   
   ## compute (x’x)^(-1) x’y
   coef <- fit$par
-  names(coef) <- names(par)
+  names(coef) <- names(start)
+  len_beta = ncol(x)
+  beta <- coef[1:len_beta]
+  par_g <- coef[(len_beta+1):(len_beta+3)]
   
   ## degrees of freedom and standard deviation of residuals
   df <- nrow(x)-ncol(x)
   require(boot) ##### put this somewhere else
-  fitted.values <- inv.logit(g_glf(v, coef[4], coef[5], coef[6]) + x%*%coef[1:3])
+  fitted.values <- inv.logit(g_glf(v, par_g) + x%*%beta)
   sigma2 <- sum((v - fitted.values)^2)/df
   
   ## compute sigma^2 * (x’x)^-1
-  vcov <- sigma2 * chol2inv(qx$qr)
-  colnames(vcov) <- rownames(vcov) <- colnames(x)
+  #vcov <- sigma2 * chol2inv(qx$qr)
+  colnames(vcov) <- rownames(vcov) <- c(colnames(x),"M","B","T")
   list(coefficients = coef,
        vcov = vcov,
        sigma = sqrt(sigma2),
@@ -86,17 +98,31 @@ contOrdEst <- function(par, v, x){
 
 contOrd <- function(x, ...) UseMethod("contOrd")
 
-contOrd.default <- function(x, v, par, ...)
+contOrd.default <- function(x, v, start=NULL, ...)
 {
     x <- as.matrix(x)
     v <- as.numeric(v)
-    est <- contOrdEst(par, v, x)
+    if (is.null(start)) beta_start <- set.beta_start(x,v)
+    len_beta = length(beta_start)
+    glf_start <- set.glf_start(x,v)
+    start <- c(beta_start, glf_start)
+    est <- contOrdEst(start, v, x)
     coef <- est$coefficients
-    est$fitted.values <- as.vector(inv.logit(g_glf(v, coef[4], coef[5], coef[6]) + x%*%coef[1:3]))
+    beta <- coef[1:len_beta]
+    par_g <- coef[(len_beta+1):(len_beta+3)]
+    est$fitted.values <- as.vector(inv.logit(g_glf(v, par_g) + x%*%beta))
     est$residuals <- v - est$fitted.values
     est$call <- match.call()
     class(est) <- "contOrd"
     est 
+}
+
+set.beta_start <- function(x,v){
+  as.numeric(-coef(glm(v~0+x,family=gaussian(link="logit"))))
+}
+
+set.glf_start <- function(x,v){
+  c(0,1,1)
 }
 
 print.contOrd <- function(x, ...)
@@ -104,13 +130,32 @@ print.contOrd <- function(x, ...)
   cat("Call:\n")
   print(x$call)
   cat("\nCoefficients:\n")
-  print(x$coefficients)
+  print(x$coefficients, ...)
 }
+
+#' @title Summarizing Continuous Ordinal Fits
+#' @description summary method for class "contOrd"
+#'
+#' @usage ## S3 method for class 'contOrd'
+#' summary(object, correlation = FALSE, symbolic.cor = FALSE, ...)
+#' ## S3 method for class 'summary.contOrd'
+#' print(x, digits = max(3, getOption("digits") - 3),
+#'      symbolic.cor = x$symbolic.cor,
+#'      signif.stars = getOption("show.signif.stars"), ...)
+
+#' @param object An object of class "contOrd", usually, a result of a call to contOrd.
+#' @param x An object of class "summary.contOrd", usually, a result of a call to summary.contOrd.
+#' @param digits The number of significant digits to use when printing.
+#' @param ... Further arguments passed to or from other methods.
+#' @keywords summary
+#' @export
+#' @examples
+#' summary.contOrd()
 
 summary.contOrd <- function(object, ...)
 {
   se <- sqrt(diag(object$vcov))
-  tval <- coef(object) / se
+  tval <- coef(object)[1:length(se)] / se
   TAB <- cbind(Estimate = coef(object),
                StdErr = se,
                t.value = tval,
@@ -126,7 +171,7 @@ print.summary.contOrd <- function(x, ...)
   cat("Call:\n")
   print(x$call)
   cat("\n")
-  printCoefmat(x$coefficients, P.value=TRUE, has.Pvalue=TRUE)
+  printCoefmat(x$coefficients, P.value=TRUE, has.Pvalue=TRUE, ...)
 }
 
 
@@ -140,6 +185,15 @@ contOrd.formula <- function(formula, data=list(), ...)
   est$formula <- formula
   est
 }
+
+#' @title Predict method for Continuous Ordinal Fits
+#' 
+#' @description Predicted values based on contOrd object.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
 
 predict.contOrd <- function(object, newdata=NULL, ...)
 {
