5b59e4966778179e5b23239c2bc36fc934d02596
maurizio-manuguerra <maurizio.manuguerra@gmail.com>
2015-02-11 16:42:32 +1100

    * First documentation for qol data set. * Reverted documentation of dg_glf to previous state (latex formula, "Test:" deleted, conflict fixed).

--- R/ordinalCont.R
+++ R/ordinalCont.R
@@ -30,3 +30,31 @@ NULL
 #' @usage data(pain)
 #' @format A data frame with 252 rows and 13 variables
 NULL
+
+#' QoL scores marked by 80 patients at five time marks (only baseline included).
+#'
+#' A dataset containing the QoL scores of 80 patients at five time marks: baseline, 2, 4, 6, 12 months after a stroke event. The variables are as follows:
+#'
+#' \itemize{
+#'   \item ID. The ID number of the patient.
+#'   \item Age Age of patient at entry in the study.
+#'   \item Gender Sex of patient.
+#'   \item Height
+#'   \item Weight
+#'   \item BMI
+#'   \item Side.Affected
+#'   \item Time...stroke duration of ...
+#'   \item Sensation ...
+#'   \item Neglect ...
+#'   \item Spasticity ...
+#'   \item Contracture ...
+#'   \item M1.EQoL.health QoL score (from 0 to 100)
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name qol
+#' @usage data(qol)
+#' @format A data frame with 80 rows and 13 variables
+NULL
+
--- R/utils.R
+++ R/utils.R
@@ -12,11 +12,7 @@ g_glf <- function(v, par){
 
 #' Derivative of generalized logistic g function
 #'
-<<<<<<< HEAD
-#' This function compute a parametric version of the g function following Richards (1959): Test: \deqn{\frac{dg(v)}{dv} = \frac{T}{B}  \frac{1}{v(1-v^{T})}}
-=======
-#' This function compute a parametric version of the g function following Richards (1959):  \deqn{\frac{dg(v)}{dv} = \alpha\frac{T}{B}  \frac{1}{v(1-v^{T})}}
->>>>>>> deb6af32909bdcfd5dad0a33153429575e96dede
+#' This function compute a parametric version of the g function following Richards (1959): \deqn{\frac{dg(v)}{dv} = \frac{T}{B}  \frac{1}{v(1-v^{T})}}
 #' @param v Vector of standarized scores from the continuous ordinal scale.
 #' @param par Vector of B, the slope of the curve, and T, the symmetry of the curve.
 #' @keywords Richards, derivative, generalized logistic function.
